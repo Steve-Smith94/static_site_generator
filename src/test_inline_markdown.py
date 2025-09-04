740ace76c2ee6384d3b2bde8dd087a7b493a478f
Steven Smith <stevexxebe@gmail.com>
2025-09-03 20:02:53 -0400

    added image and link extraction and unit tests

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links 
 from text_node import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -60,6 +60,54 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode("This has unmatched `delimiter", TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+
+
+
+    # image extraction tests
+    def test_extract_markdown_images_single(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")], matches
+        )
+
+    def test_extract_markdown_images_multiple(self):
+        text = "![a](http://a.com/x.png) and ![b cat](https://b.org/y.jpg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual(
+            [("a", "http://a.com/x.png"), ("b cat", "https://b.org/y.jpg")],
+            matches,
+        )
+
+    def test_extract_markdown_images_none(self):
+        matches = extract_markdown_images("no images here, only text")
+        self.assertListEqual([], matches)
+        
+
+    # link extraction tests
+    def test_extract_markdown_links_single(self):
+        text = "Go [to boot dev](https://www.boot.dev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+
+    def test_extract_markdown_links_multiple(self):
+        text = "See [Docs](https://docs.example.com) and [Repo](https://github.com/x)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual(
+            [("Docs", "https://docs.example.com"), ("Repo", "https://github.com/x")],
+            matches,
+        )
+
+    def test_extract_markdown_links_ignores_images(self):
+        text = "![alt](https://img.com/x.png) plus [site](https://site.com)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("site", "https://site.com")], matches)
+
+    def test_extract_markdown_links_none(self):
+        matches = extract_markdown_links("plain text no links")
+        self.assertListEqual([], matches)
 
 if __name__ == "__main__":
     unittest.main()
